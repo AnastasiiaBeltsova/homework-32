aff7c7d99d57e3e866c4253c7a68b3019f1d2473
AnastasiiaBeltsova <anastasiya.b145@gmail.com>
2023-01-23 16:04:37 +0200

    add changes

--- src/components/addTodo.js
+++ src/components/addTodo.js
@@ -1,10 +1,9 @@
 import { useDispatch } from "react-redux"
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { addItem } from "../engine/todo-reducer/thunk/addItem";
 
 
 export function AddTodo() {
-    const [disabled, setDisabled] = useState(true);
     const [inputValue, setValue] = useState("");
     const dispatch = useDispatch()
 
@@ -14,10 +13,6 @@ export function AddTodo() {
         dispatch(addItem(event, inputValue, setValue))
     }
 
-    useEffect(() => {
-        inputValue ? setDisabled(false) : setDisabled(true)
-    })
-
     return (
         <form action="#" className="form js--form" onSubmit={onSubmit}>
             <input
@@ -25,7 +20,12 @@ export function AddTodo() {
                 onChange={hadleInput}
                 type="text"
                 className="form__input js--form__input" />
-            <button className="form__button" type="submit" disabled={disabled} >Добавить</button>
+            <button
+                className="form__button"
+                type="submit"
+                disabled={!inputValue.length}>
+                Добавить
+            </button>
         </form>
     )
 }
--- src/engine/todo-reducer/thunk/deleteItems.js
+++ src/engine/todo-reducer/thunk/deleteItems.js
@@ -1,6 +1,5 @@
 import { setItems } from "../slice";
 
-export const deleteItems = () => (dispatch, state) => {
-    const { todos } = state()
-    dispatch(setItems([todos.items].slice(0, -1)))
-}
+export const deleteItems = () => (dispatch) => {
+    dispatch(setItems([]))
+} 
